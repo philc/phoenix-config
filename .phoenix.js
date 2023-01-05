cf9bbfe71a0e12140457eccc8f034e59fc1336b6
Phil Crosby <phil.crosby@gmail.com>
2023-01-04 22:08:05 -0800

    Implement app focusing
    Phew; it was hard to track down the reason why the wrong window would get focused when switching to Chrome.
    This issue plagued me in Hammerspoon as well.

--- .phoenix.js
+++ .phoenix.js
@@ -46,9 +46,42 @@ const maximizeWindow = (window) => {
   window.setFrame(destFrame);
 };
 
-Key.on("1", ["command", "control"], () => placeWindow(Window.focused(), 1, 0));
-Key.on("2", ["command", "control"], () => placeWindow(Window.focused(), 1, 1));
-Key.on("3", ["command", "control"], () => placeWindow(Window.focused(), 2, 0));
-Key.on("4", ["command", "control"], () => placeWindow(Window.focused(), 2, 1));
-Key.on("5", ["command", "control"], () => centerWindow(Window.focused()));
-Key.on("m", ["command", "control"], () => maximizeWindow(Window.focused()));
+/*
+ * Focuses the most recently accessed window of an app, if it's open. If it's not open, launch it.
+ */
+const launchOrFocus = (appName) => {
+  const  app = App.get(appName);
+  if (!app) {
+    if (!App.launch(appName, { focus: true })) {
+      Phoenix.notify(`Failed to launch app "${appName}". Maybe it's not installed?`);
+    }
+    return;
+  }
+
+  // NOTE(philc): app.windows() returns windows in the most recently-accessed order. However, sometimes
+  // the app implements its own window selection and focusing behavior when the app is activated. This is
+  // empirically the case with Google Chrome: if you try to focus a window which is not the "main window",
+  // Chrome will instead just focus the main window. This can be reproduced easily outside of Phoenix
+  // by command-tabbing to Chrome: when you do this, the main window will always be the one that gets the
+  // focus. To work around this, uncheck "Displays have separate Spaces" in System Preferences.
+
+  const window = app.windows()[0];
+  if (window) {
+    window.focus();
+  } else {
+    // The app may have no windows yet. If so, just focus the app.
+    app.focus();
+  }
+};
+
+const myModifiers = ["command", "control"];
+
+Key.on("1", myModifiers, () => placeWindow(Window.focused(), 1, 0));
+Key.on("2", myModifiers, () => placeWindow(Window.focused(), 1, 1));
+Key.on("3", myModifiers, () => placeWindow(Window.focused(), 2, 0));
+Key.on("4", myModifiers, () => placeWindow(Window.focused(), 2, 1));
+Key.on("5", myModifiers, () => centerWindow(Window.focused()));
+Key.on("m", myModifiers, () => maximizeWindow(Window.focused()));
+
+Key.on("l", myModifiers, () => launchOrFocus("Google Chrome"));
+Key.on("y", myModifiers, () => launchOrFocus("Firefox"));
